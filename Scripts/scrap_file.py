40323b35ee96827a1fa66e844aac04090d921934
anderson5692 <blueoystercult1982@gmail.com>
2021-02-13 22:59:07 -0300

    trying to use pandas

--- Scripts/scrap_file.py
+++ Scripts/scrap_file.py
@@ -1,10 +1,9 @@
 from bs4 import BeautifulSoup
-from lxml import html
 import requests
+import pandas as pd
+from lxml import html
 
 html_text = requests.get('https://pt.wikipedia.org/wiki/Atletas_mais_medalhados_nos_Jogos_Ol%C3%ADmpicos').text
 soup = BeautifulSoup(html_text, 'lxml')
 atleta = soup.find('table', class_= 'wikitable sortable').text
 print(atleta)
-
-
