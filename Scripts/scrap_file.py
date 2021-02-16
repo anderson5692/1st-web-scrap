056b1fef4cecca543f63d8e3b95f0e6e96821c34
anderson5692 <blueoystercult1982@gmail.com>
2021-02-15 22:09:11 -0300

    keep trying

--- Scripts/scrap_file.py
+++ Scripts/scrap_file.py
@@ -5,6 +5,11 @@ from lxml import html
 
 html_text = requests.get('https://pt.wikipedia.org/wiki/Atletas_mais_medalhados_nos_Jogos_Ol%C3%ADmpicos').text
 soup = BeautifulSoup(html_text, 'lxml')
-atleta = soup.find('table', class_= 'wikitable sortable').text
-print(atleta)
-print('')
+atleta = soup.find_all('table', class_= 'wikitable sortable')
+position = [p.get_text() for p in headerSort.select(".th .title")]
+nome = [n.get_text() for n in headerSort.select(".th .title")]
+country = [c.get_text() for c in headerSort.select(".th .title")]
+
+print(position)
+print(nome)
+print(country)
