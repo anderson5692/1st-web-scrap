f7480a8c733ca5fcd4080f6e75c362b98e620bb4
anderson5692 <blueoystercult1982@gmail.com>
2021-02-16 21:51:03 -0300

    steps

--- Scripts/scrap_file.py
+++ Scripts/scrap_file.py
@@ -1,15 +1,19 @@
 from bs4 import BeautifulSoup
 import requests
 import pandas as pd
-from lxml import html
 
 html_text = requests.get('https://pt.wikipedia.org/wiki/Atletas_mais_medalhados_nos_Jogos_Ol%C3%ADmpicos').text
 soup = BeautifulSoup(html_text, 'lxml')
 atleta = soup.find_all('table', class_= 'wikitable sortable')
-position = [p.get_text() for p in headerSort.select(".th .title")]
-nome = [n.get_text() for n in headerSort.select(".th .title")]
-country = [c.get_text() for c in headerSort.select(".th .title")]
+#posição,atleta,pais,ouro,prata,bronze,total
+#tupla guarda todos os dados
+records = []
+for result in results:
+    #posição --- date = result.find('strong').text[0:-1] + ', 2017'
+    #atleta --- lie = result.contents[1][1:-2]
+    # pais --- explanation = result.find('a').text[1:-1]
+    # ouro --- url = result.find('a')['href']
+    # prata --- records.append((date, lie, explanation, url))
+    # bronze
+    # total
 
-print(position)
-print(nome)
-print(country)
